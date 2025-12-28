a36d78b4da17efd65f43f78fd08a2bea57f5b41b
Daksh Tekwani <dakshtekwani2504@gmail.com>
2025-12-28 18:26:21 +0530

    Create Task 2.py

---
+++ Task 2.py
@@ -0,0 +1,20 @@
+#Task 2: Using the Math Module for Calculations
+
+import math
+    
+print("Calculating Using Math Module \n")
+number=int(input("Enter number: "))
+print("Square root: ", math.sqrt(number))
+print("Logarithm: ", math.log(number))
+print("Sine: ", math.sin(number))
+
+#Sample Output
+'''
+Calculating Using Math Module 
+
+Enter number: 25
+Square root:  5.0
+Logarithm:  3.2188758248682006
+Sine:  -0.13235175009777303
+
+'''
